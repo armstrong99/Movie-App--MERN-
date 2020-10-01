37e2e951cad52b2e61a167ae7b37854a6af9ba4b
armstrong99 <armstrong.ndukwe@gmail.com>
2020-10-01 12:15:14 +0100

    i added a function that gets the first three letters of someones name

--- ACTION/index.js
+++ ACTION/index.js
@@ -1,6 +1,14 @@
 function addName(first, second) {
 
     return `this is your full name ${first}  ${second}`
-
-    
+ 
 }
+
+function getFirstThreeLetters(name) {
+
+    return name.substring(0, 3)
+
+}
+
+console.log( getFirstThreeLetters("armstrong"))
+
